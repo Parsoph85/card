0d2090dbcc4ea39b6b0b651f5f534a30c6957a36
Andrew Tolmachev <parsoph85@gmail.com>
2025-04-26 11:38:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Card from './components/card';
 const App = () => {
   const movieData = {
     title: "Inception",
-    image: "https://via.placeholder.com/150", // Замените на реальный URL изображения
+    image: "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/8ab9a119-dd74-44f0-baec-0629797483d7/300x450", // Замените на реальный URL изображения
     description: "A mind-bending thriller about dreams within dreams."
   };
 
